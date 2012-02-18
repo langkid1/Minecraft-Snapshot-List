c858bed740e2b20d2374afa7349ef78bb21fac87
Andrew Langston <andrew@langkid1.me>
2012-02-18 14:26:38 -0600

    Completed first version for the config file writer that should work

--- pre/snapshotGetter.php
+++ pre/snapshotGetter.php
@@ -4,5 +4,44 @@
  * This page is run by a CRON job every 5 minutes to keep the snapshots.txt file up to date with the snapshot list :)
  * STILL IN DEV!
  */
-
+$snapshotsfile = fopen("snapshots.txt", "wb");
+error_reporting(E_ALL ^ E_WARNING);
+date_default_timezone_set('America/New_York');
+$curweek = date("W");
+for( ; $curweek != "0"; $curweek--)
+{
+    if(strpos($curweek, '0') === 0)
+{
+$name = "12w" . "$curweek" . "a";
+$bname = "12w" . "$curweek" . "b";
+$cname = "12w" . "$curweek" . "c";
+}
+else
+{
+$name = "12w0" . "$curweek" . "a";
+$bname = "12w0" . "$curweek" . "b";
+$cname = "12w0" . "$curweek" . "c";
+}
+//a
+$f = fopen("http://assets.minecraft.net/" . $name . "/minecraft.jar", "r");
+if($f)
+{
+    fwrite($snapshotsfile, "12w" . $curweek . "a;");
+}
+fclose($f);
+//b
+$f = fopen("http://assets.minecraft.net/" . $bname . "/minecraft.jar", "r");
+if($f)
+{
+    fwrite($snapshotsfile, "12w" . $curweek . "b;");
+}
+fclose($f);
+//c
+$f = fopen("http://assets.minecraft.net/" . $cname . "/minecraft.jar", "r");
+if($f)
+{
+    fwrite($snapshotsfile, "12w" . $curweek . "c;");
+}
+fclose($f);
+}
 ?>
